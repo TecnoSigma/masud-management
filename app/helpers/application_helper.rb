beb599fb0fbdf9d483036c14885aa307ba77f839
Alexandre Martins <alexandre.martins@locaweb.com.br>
2021-10-02 15:12:30 -0300

    Showing first user name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def first_name(token, type)
+    denomination = case type
+                   when :employee
+                     :name
+                   when :customer
+                     :company
+                   end
+
+    ServiceToken.find_by_token(token).send(type).send(denomination).split.first
+  end
+
   def convert_date_time(date_time)
     DateTime.parse(date_time.to_s).strftime('%d/%m/%Y - %H:%M')
   end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -5,6 +5,30 @@ require 'rails_helper'
 RSpec.describe ApplicationHelper, type: :helper do
   include ApplicationHelper
 
+  describe '#first_company_name' do
+    it 'returns first company name when pass customer token' do
+      first_company_name = 'ACME'
+
+      customer = FactoryBot.create(:customer, company: "#{first_company_name} S.A.")
+      service_token = FactoryBot.create(:service_token, customer: customer)
+
+      result = helper.first_name(service_token.token, :customer)
+
+      expect(result).to eq(first_company_name)
+    end
+
+    it 'returns first employee name when pass employee token' do
+      first_name = 'João'
+
+      employee = FactoryBot.create(:employee, :agent, name: "#{first_name} da Silva Santos")
+      service_token = FactoryBot.create(:service_token, employee: employee)
+
+      result = helper.first_name(service_token.token, :employee)
+
+      expect(result).to eq(first_name)
+    end
+  end
+
   describe '#convert_date_time' do
     it 'returns converted date in format dd/mm/yyyy - hh:mm:ss' do
       scheduling = FactoryBot.create(:order, :scheduled)
